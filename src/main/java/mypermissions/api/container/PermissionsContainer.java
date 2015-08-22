ff237397b8e03bf6d90f2aff66ca8747cf0d2da5
AfterWind <afterwinddesu@gmail.com>
2015-08-22 17:28:19 +0300

    Added permission removal in the permissions container.

--- src/main/java/mypermissions/api/container/PermissionsContainer.java
+++ src/main/java/mypermissions/api/container/PermissionsContainer.java
@@ -1,9 +1,11 @@
 package mypermissions.api.container;
 
 
+import mypermissions.MyPermissions;
 import mypermissions.api.entities.PermissionLevel;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class PermissionsContainer extends ArrayList<String> {
 
@@ -16,12 +18,26 @@ public class PermissionsContainer extends ArrayList<String> {
         for (String p : this) {
             if (permission.startsWith(p)) {
                 permLevel = PermissionLevel.ALLOWED;
+                //MyPermissions.instance.LOG.info("PermLevel is allowed for " + permission);
             } else if(p.startsWith("-") && permission.startsWith(p.substring(1))) {
                 permLevel = PermissionLevel.DENIED;
+                //MyPermissions.instance.LOG.info("PermLevel is denied for " + permission);
             }
         }
 
         return permLevel;
+    }
+
+
+    public boolean remove(String perm) {
+        for(Iterator<String> it = iterator(); it.hasNext(); ) {
+            String p = it.next();
+            if(p.equals(perm)) {
+                it.remove();
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
